b79a573573c4b5f3684bf5e130ce6fbbc2011562
Sergey Atroshchenko <kapxapot@gmail.com>
2024-10-20 17:58:11 +0400

    Adds analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Navbar } from "@/components/layout/navbar";
 import { ThemeProvider } from "@/components/layout/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -23,6 +25,7 @@ export default function RootLayout({
 
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
